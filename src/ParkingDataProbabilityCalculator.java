c8d91bbfa5a53e786edc3bff5f2670756d3ea441
jinukshin <jinuk@seas.upenn.edu>
2019-04-21 18:27:59 -0700

    Initial shell checkin for ParkingDataProbabilityCalculator.java class

---
+++ src/ParkingDataProbabilityCalculator.java
@@ -0,0 +1,19 @@
+
+/**
+ * This class uses data generated from ParkingTicketDataProcessor and
+ * does necessary probability calculations of various combinations for
+ * LikelyhoodPredictor to use.
+ * 
+ * @author lukeshin
+ *
+ */
+public class ParkingDataProbabilityCalculator {
+	
+	/**
+	 *  TODO: add probability calculations here by zone, hour and day information.
+	 *  Get relative zone based probabilities.
+	 *  Get day and hourly information of probabilities for a particular zone.
+	 */
+	
+
+}
